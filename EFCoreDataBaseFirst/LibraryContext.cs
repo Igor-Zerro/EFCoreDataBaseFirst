f5a14eb640d616c3c30029111aa41a2db5a283da
Kost_ce88@student.itstep.org <Kost_ce88@student.itstep.org>
2021-06-06 18:10:31 +0300

    AddTeacher

--- EFCoreDataBaseFirst/LibraryContext.cs
+++ EFCoreDataBaseFirst/LibraryContext.cs
@@ -21,13 +21,14 @@ namespace EFCoreDataBaseFirst
         public virtual DbSet<Book> Books { get; set; }
         public virtual DbSet<Group> Groups { get; set; }
         public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Teacher> Teachers { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=192.168.0.106,1433;Initial Catalog=Library;Integrated Security=False;user id=sa;password=Passw0rd%");
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=library;Integrated Security=True");
             }
         }
 
